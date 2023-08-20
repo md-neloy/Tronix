caffa6e098afa5b3823c077ad09af85b71224f87
mh neloy <mhneloy708@gmail.com>
2023-08-21 02:17:43 +0600

    complete first 8 pages

--- js/script.js
+++ js/script.js
@@ -96,6 +96,56 @@ $(".fsale_slid").slick({
   adaptiveHeight: true,
   prevArrow: "<i class='fa-solid fa-arrow-left-long pre_btn'></i>",
   nextArrow: '<i class="fa-solid fa-arrow-right-long next_btn"></i>',
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 786,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 $(".slide2").slick({
   dots: true,
@@ -160,66 +210,43 @@ $(".slide2").slick({
     // instead of a settings object
   ],
 });
-// $(".product_slide").slick({
-//   dots: true,
-//   arrows: true,
-//   infinite: true,
-//   speed: 300,
-//   slidesToShow: 4,
-//   adaptiveHeight: true,
-//   prevArrow: "<i class='fa-solid fa-arrow-left-long pre_btn'></i>",
-//   nextArrow: '<i class="fa-solid fa-arrow-right-long next_btn"></i>',
-//   responsive: [
-//     {
-//       breakpoint: 1400,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         infinite: true,
-//         dots: true,
-//       },
-//     },
-//     {
-//       breakpoint: 1200,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         infinite: true,
-//         dots: true,
-//       },
-//     },
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 3,
-//         infinite: true,
-//         dots: true,
-//       },
-//     },
-//     {
-//       breakpoint: 786,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2,
-//       },
-//     },
-//     {
-//       breakpoint: 576,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 2,
-//       },
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//       },
-//     },
-//     // You can unslick at a given breakpoint now by adding:
-//     // settings: "unslick"
-//     // instead of a settings object
-//   ],
-// });
+
+let inputs = document.querySelectorAll(".check");
+let addColors = document.querySelectorAll(".color");
+
+inputs.forEach((input) => {
+  input.addEventListener("click", element);
+});
+
+function element() {
+  inputs.forEach((input, lou) => {
+    if (input.checked) {
+      addColors[lou].classList.add("checked");
+      input.classList.add("checkedinput");
+      console.log("hello");
+    } else {
+      input.classList.remove("checkedinput");
+      addColors[lou].classList.remove("checked");
+    }
+  });
+  console.log("hello");
+}
+$(".microphone_for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".microphone_nav",
+});
+$(".microphone_nav").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: ".microphone_for",
+  dots: false,
+  centerMode: false,
+  focusOnSelect: true,
+  prevArrow:
+    '<button type="button" class="slick-prev pre_btn"><i class="fa-solid fa-chevron-left"></i></button>',
+  nextArrow:
+    '<button type="button" class="slick-next nxt_btn"><i class="fa-solid fa-chevron-right"></i></button>',
+});
